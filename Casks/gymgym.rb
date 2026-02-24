5c43f6d3681ba03191fc219b4c28888264af040f
Andrew jeon <andrew.jeon@box.co.kr>
2026-02-24 23:15:02 +0900

    Update GymGym to v1.0

---
+++ Casks/gymgym.rb
@@ -0,0 +1,23 @@
+cask "gymgym" do
+  version "1.0"
+  sha256 "05d35fda975d7f1d665f9dad7b986eecf3963bfd63296cdf26b6254ce07f8b90"
+
+  url "https://github.com/box-kr/gymgym/releases/download/v#{version}/GymGym-#{version}.zip"
+  name "GymGym"
+  desc "macOS Menu Bar Utility for monitoring system resources and displaying animated runners"
+  homepage "https://github.com/box-kr/gymgym"
+
+  app "GymGym.app"
+
+  # 다운로드 후 Gatekeeper에서 '손상된 앱'으로 인식되는 것을 방지합니다.
+  postflight do
+    system_command "xattr",
+                   args: ["-cr", "#{appdir}/GymGym.app"],
+                   sudo: false,
+                   must_succeed: false
+  end
+
+  zap trash: [
+    "~/Library/Preferences/com.andrew.GymGym.plist",
+  ]
+end
